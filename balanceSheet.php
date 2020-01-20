d6c426f677634a4b2943cdbd71aa3c8e9eb01aaf
TripathiRaghu <tripathiraghu1@gmail.com>
2020-01-21 00:58:30 +0530

    members list to balance sheet

--- balanceSheet.php
+++ balanceSheet.php
@@ -1,9 +1,9 @@
 <?php
+ob_start();
 session_start();
-include 'connection.php';
+$name=$_GET['nm'];
 
 ?>
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,7 +12,7 @@ include 'connection.php';
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="generator" content="">
-    <title>Members | Ledger</title>
+    <title>Balance Sheet | Ledger</title>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
@@ -20,59 +20,6 @@ include 'connection.php';
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <link rel="icon" href="image/icon.png">
-<style type="text/css">
-  <!-- styling of the list -->
-  body{
-
-
-  }
-
-  .container .list{
-    background-color: white;
-    border:0.5px solid lightgrey;
-    margin-top: 12px;
-    padding: 10px;
-    border-radius: 20px;
-    display: flex;
-  }
-  .container .list .listSub {
-    display: block;
-
-  }
-  .container .list .listSub .mobile_number{
-    font-size: 14px;
-    padding-left: 8px;
-    /*padding-top: 10px;*/
-    margin:0px;
-    display: block;
-  }
-  .container .list .listSub .userName{
-    font-size: 20px;
-    padding-left: 10px;
-    /*padding-top: 5px;*/
-    margin:0px;
-    display: block;
-  }
-  .container .list .profileImage{
-    border-radius: 20px;
-  }
-
-  .container .list .amount{
-    color: green;
-    font-size: 22px;
-    margin: 0px;
-    padding-right: 0px;
-    margin-right: 0px;
-    float: right;
-    @include float-right;
-
-  }
-  .float-right{
-    width: -moz-available;
-  }
-
-</style>
-</head>
 <body style="background-color: ghostwhite;">
 <!-- navbar -->
 
@@ -96,69 +43,12 @@ include 'connection.php';
         
       </div>
     </nav>
-
-<!-- navebar Ends -->
-
-<!-- members List -->
-
-<div class="container" style="margin-top: 100px;">
-
-		
-<?php
-// fetching the user list
-$sql_tab="SELECT * FROM members";
-$dt=mysqli_query($con,$sql_tab);
-$a=mysqli_num_rows($dt);
-
-if ($a>0)
-{
-  	while($row= mysqli_fetch_array($dt,MYSQLI_ASSOC))
-{
-
-
-$id = $row['id'];
-
-$userName=$row['name'];
-
-$email=$row['email'];
-$mobile=$row['mobile_number'];
-$profileImage=$row['profileImage'];
-
-// printing the user list
-
-
-
-// main styling start
-
-echo "<a href=balanceSheet.php?nm=".$row['name'].">";
-echo "<div class="." list".">";
-echo "<img src=".$profileImage." width="."50"." height="."50"." class="."profileImage".">";
-echo "<div class="." listSub".">";
-echo "<p class="."userName".">". $userName."</p>";
-echo "<p class="."mobile_number"."><a href="."tel:". $mobile.">". $mobile."</a></p>";
-echo "</div>";
-echo "<div class="."float-right".">";
-echo "<p class="."amount"." >+25.00</p>";
-echo "<br>";
-echo "</div>";
-echo "</div>";
-
-echo "</a>";
-}//closing while
-
-}//closiung if
-
-?>
+<!-- user name -->
+<div class="container">
+<h2 style="margin-top: 100px;" align="center"><?php  echo $name; ?></h2>
 
 </div>
-
-
-
-
-
-
+<div class="container"></div>
 
 </body>
-
-
 </html>
