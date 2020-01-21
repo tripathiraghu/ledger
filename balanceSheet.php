84eac735e9f4743a3db4f76408d0c9100a3bed8b
TripathiRaghu <tripathiraghu1@gmail.com>
2020-01-21 23:20:00 +0530

    balance sheet page config done
    here i done the design and configration of the balance sheet

--- balanceSheet.php
+++ balanceSheet.php
@@ -2,10 +2,10 @@
 ob_start();
 include 'connection.php';
 session_start();
-$id=$_GET['nm'];
+$userId=$_GET['nm'];
 
 // fetching the user detail current user details
-$sql_tab="SELECT * FROM members where id='$id'";
+$sql_tab="SELECT * FROM members where id='$userId'";
 $dt=mysqli_query($con,$sql_tab);
 $a=mysqli_num_rows($dt);
 
@@ -30,11 +30,6 @@ $profileImage=$row['profileImage'];
 ?>
 
 
-
-
-
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -48,10 +43,30 @@ $profileImage=$row['profileImage'];
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
 <link rel="icon" href="image/icon.png">
+<style type="text/css">
+	.credit-Box{
+		color: black;
+		border-radius: 20px;
+		border: 1px solid #e8e8e8;
+		width: auto;
+		background-color: #e8e8f5;
+		margin: 10px 0px 0px 10px;
+	}
+
+	.debit-Box{
+		color: black;
+		border-radius: 20px;
+		border: 1px dashed #e8e8e8;
+		width: auto;
+		background-color:white;
+		margin-right: 10px;
+		/*margin: 10px 0px 0px 10px;*/
+	}
+
+
+</style>
 <body style="background-color: ghostwhite;">
 <!-- navbar -->
 
@@ -80,14 +95,15 @@ $profileImage=$row['profileImage'];
 
 <div class="row">
 	<!-- profile picture -->
-	<div class="col-3">
+	<div class="col-2">
 		<img src="<?php  echo $profileImage; ?>" width="50" height="50" style="border-radius: 20px;">
 	</div>
 
 
 	<!-- profile Name -->
-	<div class="col-6">
-		<p style="padding-top: 10px;" align="center"><?php  echo $userName; ?></p>
+	<div class="col-7">
+		<p style="padding-top: 0px; font-size: 20px; margin: 0px;"><?php  echo $userName; ?></p>
+		<p style=" font-size: 12px;"><?php  echo $mobile; ?></p>
 </div>
 	<!-- call button -->
 	<div class="col-3" align="center">
@@ -96,24 +112,101 @@ $profileImage=$row['profileImage'];
 		</a>
 </div>
 </div>
+<hr style="margin: 0px;">
 </div>
 
+<!-- transactions box -->
 <div class="container">
-	
+	<?php
+// fetching the user list
+$sql_tab="SELECT * FROM transaction where id='$userId'";
+$dt=mysqli_query($con,$sql_tab);
+$a=mysqli_num_rows($dt);
+
+if ($a>0)
+{
+  	while($row= mysqli_fetch_array($dt,MYSQLI_ASSOC))
+{
+
+
+$id = $row['id'];
+$transactionType= $row['type'];
+$transactionAmount = $row['amount'];
+$transactionResaon = $row['reason'];
+$transactionDate = $row['date'];
+// credit box
+if ($transactionType=="Credit") {
+	?>
+<div class="row">
+<div class="container credit-Box">
+	<div style="display: flex;margin-bottom: 0px;"> 
+	<p style="padding-top: 10px; padding-right: 10px; margin-bottom: 5px;">&#8377;  </p><h2 style="margin-bottom: 5px;"><?php echo $transactionAmount ?></h2>
+	</div>
+	<p style="font-size: 14px; margin-bottom: 5px;"><?php echo $transactionResaon ?></p>
+	<p style="font-size: 14px;">Credited  
+<?php echo $transactionDate ?> <i class="fa fa-long-arrow-right" aria-hidden="true"></i> </p>
 </div>
+</div>
+<?php	
+} else{
+	// Debit box
+	?>
+
+<div class="row">
+<div class="container debit-Box">
+	<div style="display: flex;margin-bottom: 0px;"> 
+	<p style="padding-top: 10px; padding-right: 10px; margin-bottom: 5px;">&#8377;  </p><h2 style="margin-bottom: 5px;"><?php echo $transactionAmount ?></h2>
+	</div>
+	<p style="font-size: 14px; margin-bottom: 5px;"><?php echo $transactionResaon ?></p>
+	<p style="font-size: 14px;"><i class="fa fa-long-arrow-left" aria-hidden="true"> </i> Debited 
+<?php echo $transactionDate ?></p>
+</div>
+</div>
+	
+<?php
+}
+
+
+
+
+
+
+}//closing while
+
+}//closiung if
+
+?>
+
+</div>
+
+
+<!-- base buttons -->
 <div class="baseButton fixed-bottom container" style="margin-bottom: 20px;">
   <div class="row">
   <!-- credit -->
   <div class="col-6">
+<?php
+  	echo "<a href=credit.php?nm=".$id.">";?>
     <button type="button" class="btn btn-success btn-block"><i class="fa fa-arrow-down" aria-hidden="true"></i></i> Credit</button>
+    <?php echo "</a>" ?>
   </div>
 <!-- debit -->
 <div class="col-6">
+	<?php
+	echo "<a href=debit.php?nm=".$id."";?>
     <button type="button" class="btn btn-danger btn-block"><i class="fa fa-arrow-up" aria-hidden="true"></i> Debit</button>
+    <?php echo "</a>" ?>
   </div>
 
   </div>
 </div>
 
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ credit.php
@@ -0,0 +1,16 @@
+<?php
+ob_start();
+include 'connection.php';
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add Credit</title>
+</head>
+<body>
+
+</body>
+</html>
